0b38f14edd271317fbb3d0db01708a717cb0c6b3
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-01 09:56:27 -0700

    cleaned up display function, remove working properly, a couple of new tests

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,11 +1,12 @@
 # _*_ coding: utf-8 _*_
 import pytest
+from src.linked_list import LinkedList
 
 
 VAL_LIST = [
-    ([2, 3, 4], "(4, 3, 2)"),
-    ([4, 2, 3], "(3, 2, 4)"),
-    (['test', 'makes', 'angry'], "('angry', 'makes', 'test')"),
+    ([2, 3, 4], "(4, 3, 2,)"),
+    ([4, 2, 3], "(3, 2, 4,)"),
+    (['test', 'makes', 'angry'], "('angry', 'makes', 'test',)"),
 ]
 
 SINGLE_VAL = [
@@ -16,33 +17,28 @@ SINGLE_VAL = [
 
 
 def test_LinkedList():
-    from src.linked_list import LinkedList
     assert type(LinkedList) == type
 
 
 def test_LinkedList_2():
-    from src.linked_list import LinkedList
     test_empty = LinkedList()
     assert test_empty.values == []
 
 
 @pytest.mark.parametrize('a', VAL_LIST)
 def test_LinkedList_3(a):
-    from src.linked_list import LinkedList
     test_values = LinkedList(a)
     assert test_values.values == a
 
 
 @pytest.mark.parametrize('a, b', VAL_LIST)
 def test_LinkedList_4(a, b):
-    from src.linked_list import LinkedList
     test_values = LinkedList(a)
     assert test_values.display() == b
 
 
 @pytest.mark.parametrize('a', SINGLE_VAL)
 def test_LinkedList_insert(a):
-    from src.linked_list import LinkedList
     this_list = LinkedList([1])
     this_list.insert(a)
     assert this_list.head.data == a
@@ -50,21 +46,38 @@ def test_LinkedList_insert(a):
 
 @pytest.mark.parametrize('a', VAL_LIST)
 def test_LinkedList_size(a):
-    from src.linked_list import LinkedList
     test_values = LinkedList(a)
     assert test_values.size() == len(a)
 
 
 @pytest.mark.parametrize('a', VAL_LIST)
 def test_LinkedList_search(a):
-    from src.linked_list import LinkedList
     test_values = LinkedList(a)
     assert test_values.search(test_values.head.data) == test_values.head
 
 
 @pytest.mark.parametrize('a', SINGLE_VAL)
 def test_LinkedList_remove_2(a):
-    from src.linked_list import LinkedList
     this_list = LinkedList(a)
     this_list.remove(this_list.head)
     assert this_list.size() == 0
+
+
+def test_remove_one():
+    lst = LinkedList([x for x in xrange(10)])
+    node = lst.head.next_node.next_node
+    lst.remove(node)
+    assert lst.size() == 9
+
+
+def test_display():
+    lst = LinkedList([x for x in xrange(10)])
+    assert lst.display() == "(9, 8, 7, 6, 5, 4, 3, 2, 1, 0,)"
+
+
+def test_display2():
+    lst = LinkedList(['foo', 'sarah'])
+    assert lst.display() == "('sarah', 'foo',)"
+
+
+
