7b4c7117e774c2a781b552e5edc7ac14801072f0
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-02 02:50:30 -0700

    no longer importing the linked list

--- src/double_linked_list.py
+++ src/double_linked_list.py
@@ -1,8 +1,5 @@
-# from linked_list import LinkedList
-
-
 class DblNode(object):
-    """Make  Node Object with data, next, and previous node."""
+    """Make Node Object with data, next, and previous node."""
 
     def __init__(self, data=None, next_node=None, previous_node=None):
         """Init Node with optional args Data, Next_node, Previous_node."""
@@ -12,7 +9,8 @@ class DblNode(object):
 
 
 class DblList(object):
-    """Create a list of doubley linked nodes"""
+    """Create a list of doubley linked nodes."""
+
     def __init__(self, values=[]):
         """Initialize New instance of Doubley LinkedList."""
         self.values = values
@@ -24,10 +22,10 @@ class DblList(object):
         """Insert Node at the front (head) of list."""
         inserted_node = DblNode(val, self.head)
         if not self.head:
+            self.head = self.tail = inserted_node
+        else:
+            self.head.previous_node = inserted_node
             self.head = inserted_node
-            self.tail = self.head
-        self.head.previous_node = inserted_node
-        self.head = inserted_node
 
     def append(self, val):
         """Append New node to back (tail) of list."""
@@ -38,20 +36,14 @@ class DblList(object):
     def pop(self):
         """Remove Node from Front (head) of list."""
         popped_node = self.head
-        # popped = self.head.data
-        # new_head = self.head.next_node
-        # self.head = new_head
         self.head = popped_node.next_node
         self.head.previous_node = None
         return popped_node.data
 
     def shift(self):
         """Remove Node from Back (Tail) of list."""
-        # shifted = self.tail.data
         shifted = self.tail
-        # self.tail = self.tail.previous_node
         self.tail = shifted.previous_node
-        # return shifted
         return shifted.data
 
     def remove(self, node):
@@ -67,9 +59,7 @@ class DblList(object):
                 try:
                     if current.next_node == target_node:
                         next_node = current.next_node
-                        # current.next_node = target_node.next_node
                         next_node = target_node.next_node
-                        # target_node = current.next_node
                         next_node.previous_node = current.previous_node
                         break
                     current = current.next_node
